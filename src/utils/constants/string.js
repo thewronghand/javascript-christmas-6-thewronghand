d1fc3c0ac46f6d49d0447e220712853fa35ef2d2
thewronghand <penfreak77@gmail.com>
2023-11-15 13:48:51 +0900

    refactor: 배열 내의 각 객체를 freeze

--- src/utils/constants/string.js
+++ src/utils/constants/string.js
@@ -7,20 +7,22 @@ const menuCategories = Object.freeze({
   drink: '음료',
 });
 
-const menuItems = Object.freeze([
-  { category: menuCategories.appetizer, name: '양송이수프', price: 6000 },
-  { category: menuCategories.appetizer, name: '타파스', price: 5500 },
-  { category: menuCategories.appetizer, name: '시저샐러드', price: 8000 },
-  { category: menuCategories.main, name: '티본스테이크', price: 55000 },
-  { category: menuCategories.main, name: '바비큐립', price: 54000 },
-  { category: menuCategories.main, name: '해산물파스타', price: 35000 },
-  { category: menuCategories.main, name: '크리스마스파스타', price: 25000 },
-  { category: menuCategories.dessert, name: '초코케이크', price: 15000 },
-  { category: menuCategories.dessert, name: '아이스크림', price: 5000 },
-  { category: menuCategories.drink, name: '제로콜라', price: 3000 },
-  { category: menuCategories.drink, name: '레드와인', price: 60000 },
-  { category: menuCategories.drink, name: '샴페인', price: 25000 },
-]);
+const menuItems = Object.freeze(
+  [
+    { category: menuCategories.appetizer, name: '양송이수프', price: 6000 },
+    { category: menuCategories.appetizer, name: '타파스', price: 5500 },
+    { category: menuCategories.appetizer, name: '시저샐러드', price: 8000 },
+    { category: menuCategories.main, name: '티본스테이크', price: 55000 },
+    { category: menuCategories.main, name: '바비큐립', price: 54000 },
+    { category: menuCategories.main, name: '해산물파스타', price: 35000 },
+    { category: menuCategories.main, name: '크리스마스파스타', price: 25000 },
+    { category: menuCategories.dessert, name: '초코케이크', price: 15000 },
+    { category: menuCategories.dessert, name: '아이스크림', price: 5000 },
+    { category: menuCategories.drink, name: '제로콜라', price: 3000 },
+    { category: menuCategories.drink, name: '레드와인', price: 60000 },
+    { category: menuCategories.drink, name: '샴페인', price: 25000 },
+  ].map(item => Object.freeze(item)),
+);
 
 const MENU = Object.freeze({
   menuCategories,
@@ -33,7 +35,9 @@ const BADGES = Object.freeze(
     { threshold: 10000, badgeName: '트리' },
     { threshold: 5000, badgeName: '별' },
     { threshold: 0, badgeName: '없음' },
-  ].sort((a, b) => b.threshold - a.threshold),
+  ]
+    .sort((a, b) => b.threshold - a.threshold)
+    .map(item => Object.freeze(item)),
 );
 
 const SYMBOLS = Object.freeze({
