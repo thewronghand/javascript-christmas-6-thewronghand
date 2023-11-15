b7d36e1959ac73bf6a7d095248addcb78b07a515
thewronghand <penfreak77@gmail.com>
2023-11-15 13:48:30 +0900

    refactor: 메세지의 '12월'을 동적으로 정의
    확장성을 위해 (다른 달에도 사용할 수 있도록) 현재 달을 동적으로 표기한다.

--- src/utils/constants/message.js
+++ src/utils/constants/message.js
@@ -1,3 +1,5 @@
+import DATE from './date';
+
 const format = Object.freeze({
   menu: (menuName, count) => `${menuName} ${count}개`,
   number: number => number.toLocaleString('ko-KR'),
@@ -9,10 +11,13 @@ const format = Object.freeze({
 
     return `${month}월 ${day}일`;
   },
+  month: month => month + 1,
 });
 
 const header = Object.freeze({
-  application: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
+  application: `안녕하세요! 우테코 식당 ${format.month(
+    DATE.month,
+  )}월 이벤트 플래너입니다.`,
   benefitAnnouncement: date =>
     `${format.date(date)}에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
   orders: '<주문 메뉴>',
@@ -21,12 +26,13 @@ const header = Object.freeze({
   benefitDetails: '<혜택 내역>',
   totalBenefitAmount: '<총혜택 금액>',
   expectedPaymentAfterDiscount: '<할인 후 예상 결제 금액>',
-  eventBadge: '<12월 이벤트 배지>',
+  eventBadge: `<${format.month(DATE.month)}월 이벤트 배지>`,
 });
 
 const read = Object.freeze({
-  dateInput:
-    '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+  dateInput: `${format.month(
+    DATE.month,
+  )}월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)`,
   ordersInput:
     '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
 });
